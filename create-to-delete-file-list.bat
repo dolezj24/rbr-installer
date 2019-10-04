809f95b3cf4b482ee179366cae2999d98e64fe3b
Jakub Doležal <jkbdolezal@gmail.com>
2019-10-04 15:54:39 +0200

    To delete script fix

--- create-to-delete-file-list.bat
+++ create-to-delete-file-list.bat
@@ -1,8 +1,8 @@
 @echo off
 echo !macro UNINSTALL_FILES > to-delete.nsh
 dir /b /a-d rbr-files > temp.txt
-(for /f %%a in (temp.txt) do @echo Delete $INSTDIR\%%a >> to-delete.nsh)
+(for /f "delims=" %%a in (temp.txt) do @echo Delete $INSTDIR\%%a >> to-delete.nsh)
 dir /b /ad rbr-files > temp.txt
-(for /f %%a in (temp.txt) do @echo RMDir /r $INSTDIR\%%a >> to-delete.nsh)
+(for /f "delims=" %%a in (temp.txt) do @echo RMDir /r $INSTDIR\%%a >> to-delete.nsh)
 echo !macroend >> to-delete.nsh
 del temp.txt
